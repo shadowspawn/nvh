47c29052970d976e5aa95a8b5eb463777a39c11f
John Gee <j.gee@adinstruments.com>
2020-01-27 21:55:21 +1300

    is_numeric_version

---
+++ sh/is_numeric_version.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# function is_numeric_version() {
+#   # e.g. 6, v7.1, 8.11.3
+#   [[ "$1" =~ ^[v]{0,1}[0-9]+(\.[0-9]+){0,2}$ ]]
+# }
+
+is_numeric_version() {
+  printf "%s" "$1" | grep -E '^v{0,1}\d+(\.\d+){0,2}$' >/dev/null
+}
+
+if is_numeric_version "$1"; then
+  echo Numeric
+else
+  echo not
+fi
