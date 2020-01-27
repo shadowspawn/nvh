27ec6c30538a190c22a1fc5c5bfcbe65d6b5cd97
John Gee <j.gee@adinstruments.com>
2020-01-27 22:03:27 +1300

    eschew shorthand, although may be standard

--- sh/is_numeric_version.sh
+++ sh/is_numeric_version.sh
@@ -6,7 +6,7 @@
 # }
 
 is_numeric_version() {
-  printf "%s" "$1" | grep -E '^v{0,1}\d+(\.\d+){0,2}$' >/dev/null
+  printf "%s" "$1" | grep -E '^v{0,1}[0-9]+(\.[0-9]+){0,2}$' >/dev/null
 }
 
 if is_numeric_version "$1"; then
